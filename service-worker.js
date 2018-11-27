c6f8f65b0839cc8e2e81e7695095cdc1ee26b65c
Rafael Rinaldi <rafael@rinaldi.io>
2018-11-27 15:58:36 -0500

    CV updates.

--- service-worker.js
+++ service-worker.js
@@ -14,11 +14,17 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v10'
+const PRECACHE = 'precache-v12'
 const RUNTIME = 'runtime'
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = ['./']
+const PRECACHE_URLS = [
+  './T4c.woff',
+  './T4c.woff2',
+  './favicon.ico',
+  './index.html',
+  './cv.html'
+]
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
