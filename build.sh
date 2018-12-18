1a0f1b62e89a0b5d9b4e3affe5c16d1b91f4fe12
Rafael Rinaldi <rafael@rinaldi.io>
2018-12-17 19:24:29 -0500

    A bunch of improvements on the build pipeline

--- build.sh
+++ build.sh
@@ -4,19 +4,36 @@ cp $template /tmp
 tempfile="/tmp/$template"
 echo "Temporary HTML file: $tempfile"
 echo "Downloading Font Face Observer..."
-loadFonts=`curl -Ls https://unpkg.com/fontfaceobserver`
+loadFonts=`curl -Ls https://unpkg.com/fontfaceobserver/fontfaceobserver`
 echo "Done."
 echo "Generating HTML meta tags..."
 metatags=`cat data.json | html-meta-tags`
 echo "Gathering email..."
-emailSecret=`echo $EMAIL | sed -e "s/@/\[at\]/g" -e "s/\./\[dot\]/g"`
+emailSecret=`echo ${EMAIL:-} | sed -e "s/@/\[at\]/g" -e "s/\./\[dot\]/g"`
 echo "Done."
 echo "Replacing variables..."
-replace -s "{metatags}" "$metatags" $tempfile
-replace -s "{loadFonts}" "$loadFonts" $tempfile
-replace -s "{email}" "$EMAIL" $tempfile
-replace -s "{emailSecret}" "$emailSecret" $tempfile
+replace --silent "{metatags}" "$metatags" $tempfile
+replace --silent "{loadFonts}" "$loadFonts" $tempfile
+replace --silent "{email}" "${EMAIL:-}" $tempfile
+replace --silent "{emailSecret}" "$emailSecret" $tempfile
 echo "Done."
 echo "Compressing HTML file..."
-htmlmin $tempfile > index.html
+html-minifier --collapse-boolean-attributes \
+              --collapse-whitespace  \
+              --remove-attribute-quotes \
+              --remove-attribute-quotes  \
+              --remove-comments  \
+              --remove-empty-attributes \
+              --remove-empty-elements \
+              --remove-optional-tags  \
+              --remove-redundant-attributes  \
+              --remove-script-type-attributes  \
+              --remove-tag-whitespace \
+              --sort-attributes \
+              --sort-class-name \
+              --use-short-doctype  \
+              --minify-css true \
+              --minify-js true \
+              --output index.html \
+              $tempfile
 echo "Done."
