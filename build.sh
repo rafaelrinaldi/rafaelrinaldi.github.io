67701642b17a32a63284bf09ce1f80366ea13198
Rafael Rinaldi <rafael@rinaldi.io>
2020-04-30 22:00:23 -0400

    30

--- build.sh
+++ build.sh
@@ -1,39 +1,50 @@
-echo "Generating temporary HTML file..."
-template="index.dev.html"
-cp $template /tmp
-tempfile="/tmp/$template"
-echo "Temporary HTML file: $tempfile"
-echo "Downloading Font Face Observer..."
-loadFonts=`curl -Ls https://unpkg.com/fontfaceobserver/fontfaceobserver`
-echo "Done."
 echo "Generating HTML meta tags..."
 metatags=`cat data.json | html-meta-tags`
+
+echo "Downloading Font Face Observer..."
+loadFonts=`curl -Ls https://unpkg.com/fontfaceobserver/fontfaceobserver`
+
 echo "Gathering email..."
 emailSecret=`echo ${EMAIL:-} | sed -e "s/@/\[at\]/g" -e "s/\./\[dot\]/g"`
-echo "Done."
-echo "Replacing variables..."
-replace --silent "{metatags}" "$metatags" $tempfile
-replace --silent "{loadFonts}" "$loadFonts" $tempfile
-replace --silent "{email}" "${EMAIL:-}" $tempfile
-replace --silent "{emailSecret}" "$emailSecret" $tempfile
-echo "Done."
-echo "Compressing HTML file..."
-html-minifier --collapse-boolean-attributes \
-              --collapse-whitespace  \
-              --remove-attribute-quotes \
-              --remove-attribute-quotes  \
-              --remove-comments  \
-              --remove-empty-attributes \
-              --remove-empty-elements \
-              --remove-optional-tags  \
-              --remove-redundant-attributes  \
-              --remove-script-type-attributes  \
-              --remove-tag-whitespace \
-              --sort-attributes \
-              --sort-class-name \
-              --use-short-doctype  \
-              --minify-css true \
-              --minify-js true \
-              --output index.html \
-              $tempfile
-echo "Done."
+
+echo "Processing template files:"
+ls *.dev.html
+
+for template in *.dev.html
+do
+  echo "\nGenerating temporary HTML file..."
+  cp $template /tmp
+  tempfile="/tmp/$template"
+  echo "Temporary HTML file: $tempfile"
+
+  echo "Replacing variables..."
+  replace --silent "{metatags}" "$metatags" $tempfile
+  replace --silent "{loadFonts}" "$loadFonts" $tempfile
+  replace --silent "{email}" "${EMAIL:-}" $tempfile
+  replace --silent "{emailSecret}" "$emailSecret" $tempfile
+
+  output=`basename $template | sed -e 's/\.dev//'`
+
+  echo "Compressing HTML file..."
+  html-minifier --collapse-boolean-attributes \
+                --collapse-whitespace  \
+                --remove-attribute-quotes \
+                --remove-attribute-quotes  \
+                --remove-comments  \
+                --remove-empty-attributes \
+                --remove-empty-elements \
+                --remove-optional-tags  \
+                --remove-redundant-attributes  \
+                --remove-script-type-attributes  \
+                --remove-tag-whitespace \
+                --sort-attributes \
+                --sort-class-name \
+                --use-short-doctype  \
+                --minify-css true \
+                --minify-js true \
+                --output $output \
+                $tempfile
+  echo "Generated file '$output'"
+done
+
+echo "Done"
